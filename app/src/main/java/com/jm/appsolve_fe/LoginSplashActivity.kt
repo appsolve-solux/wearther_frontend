2075976f897d765427528a959109d1ba422b1c1f
ChoiSeoY <chltjdud30@naver.com>
2025-01-05 16:04:13 +0900

    feat: splash 화면 추가

---
+++ app/src/main/java/com/jm/appsolve_fe/LoginSplashActivity.kt
@@ -0,0 +1,21 @@
+package com.jm.appsolve_fe
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_splash)
+
+        Log.d("Splash", "Splash started")
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
