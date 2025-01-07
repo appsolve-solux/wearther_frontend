e6d814c3e6ed9fa35ca5627d5665cd44ae8dd8aa
ChoiSeoY <chltjdud30@naver.com>
2025-01-07 23:46:10 +0900

    feat: 회원가입 (이메일 입력)

--- app/src/main/java/com/jm/appsolve_fe/LoginSignUpEmailActivity.kt
+++ app/src/main/java/com/jm/appsolve_fe/LoginSignUpEmailActivity.kt
@@ -1,13 +1,16 @@
 package com.jm.appsolve_fe
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginSignUpEmailActivity : AppCompatActivity() {
@@ -18,6 +21,7 @@ class LoginSignUpEmailActivity : AppCompatActivity() {
 
         val spinnerEmailDomain = findViewById<Spinner>(R.id.spinnerEmailDomain)
         val etEmail = findViewById<EditText>(R.id.etEmail)
+        val emailBtnNext = findViewById<Button>(R.id.emailBtnNext)
 
         val emailDomains = resources.getStringArray(R.array.email_domains).toList()
 
@@ -30,14 +34,49 @@ class LoginSignUpEmailActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerEmailDomain.adapter = adapter
 
+        // Spinner 선택 상태 변수
+        var selectedDomainValid = false
+
+        // Spinner 이벤트 처리
         spinnerEmailDomain.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedDomain = emailDomains[position]
+                selectedDomainValid = position != 0
+                updateButtonState(etEmail.text.toString(), selectedDomainValid, emailBtnNext)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                //
+                selectedDomainValid = false
+                updateButtonState(etEmail.text.toString(), selectedDomainValid, emailBtnNext)
             }
+        }
+
+        // EditText 입력 상태 확인
+        etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateButtonState(s.toString(), selectedDomainValid, emailBtnNext)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // 버튼 클릭 이벤트 처리
+        emailBtnNext.setOnClickListener {
+            if (emailBtnNext.isEnabled) {
+                val intent = Intent(this, LoginSignUpIdActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    // 버튼 활성화 상태 업데이트
+    private fun updateButtonState(email: String, isDomainValid: Boolean, button: Button) {
+        if (email.isNotEmpty() && isDomainValid) {
+            button.isEnabled = true
+            button.setBackgroundTintList(getColorStateList(R.color.purple))
+        } else {
+            button.isEnabled = false
+            button.setBackgroundTintList(getColorStateList(R.color.gray_9E9E9E))
         }
     }
 }
---
+++ app/src/main/java/com/jm/appsolve_fe/LoginSignUpIdActivity.kt
@@ -0,0 +1,12 @@
+package com.jm.appsolve_fe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginSignUpIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_sign_up_id)
+
+    }
+}
