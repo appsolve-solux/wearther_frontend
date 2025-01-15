2c4bfcacf4b5893b8c26dbf85f340d60118351a6
ChoiSeoY <chltjdud30@naver.com>
2025-01-15 18:51:58 +0900

    feat: 회원가입 (체질 유형 선택)

---
+++ app/src/main/java/com/jm/appsolve_fe/LoginPickTopActivity.kt
@@ -0,0 +1,11 @@
+package com.jm.appsolve_fe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginPickTopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_pick_top)
+    }
+}
