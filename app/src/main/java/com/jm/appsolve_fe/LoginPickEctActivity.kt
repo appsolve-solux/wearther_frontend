ddc0567933e9a4998b44a45f24d7cb0f897c93a1
ChoiSeoY <chltjdud30@naver.com>
2025-01-15 22:52:06 +0900

    feat: 회원가입 (보유한 옷-하의 선택)

---
+++ app/src/main/java/com/jm/appsolve_fe/LoginPickEctActivity.kt
@@ -0,0 +1,12 @@
+package com.jm.appsolve_fe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginPickEctActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_pick_ect)
+
+    }
+}
