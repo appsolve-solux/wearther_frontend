778ab0938a8924b7e61ac21eeebd60f46e41bc55
ChoiSeoY <chltjdud30@naver.com>
2025-02-01 23:18:50 +0900

    feat: nav

--- app/src/main/java/com/jm/appsolve_fe/MainActivity.kt
+++ app/src/main/java/com/jm/appsolve_fe/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.jm.appsolve_fe
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.jm.appsolve_fe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+        replaceFragment(Home())  // 첫화면 home으로 설정
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.closet -> replaceFragment(Closet())
+                R.id.mypage -> replaceFragment(Mypage())
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
