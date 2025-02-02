51e29155c03f99dcb13805b4e19f2b5436ec618c
ChoiSeoY <chltjdud30@naver.com>
2025-02-02 13:42:50 +0900

    feat: 비밀번호 변경

--- app/src/main/java/com/jm/appsolve_fe/PasswordEditActivity.kt
+++ app/src/main/java/com/jm/appsolve_fe/PasswordEditActivity.kt
@@ -1,11 +1,27 @@
 package com.jm.appsolve_fe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class PasswordEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_edit)
+
+        val backBtn = findViewById<ImageView>(R.id.backBtn)
+
+        backBtn.setOnClickListener{
+            val intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val closeBtn = findViewById<ImageView>(R.id.closeBtn)
+
+        closeBtn.setOnClickListener {
+            finishAffinity()
+        }
     }
 }
